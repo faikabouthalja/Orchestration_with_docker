fbd6f42ff8479f6c314cade8994c62da256e21b7
faika_bouthalja <faika_bouthalja@carrefour.com>
2018-03-01 16:23:42 +0000

    tests ok on dev

---
+++ afterscriptbo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+until docker logs borecrute_phpbo_1  | grep -m 1 "fpm is running"; do echo "still running ...." && sleep 4 ; done
+echo "found"
+rm -rf /datadrive/srcrecrute/app/cache/*
+rm -rf  /datadrive/srcrecrute/app/logs/*
+rm -rf  /datadrive/logsrecrute/symfony/*
+chmod -R 777 /datadrive/srcrecrute/app/cache/
+chmod -R 777  /datadrive/srcrecrute/app/logs/
